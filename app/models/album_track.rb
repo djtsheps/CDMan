1f0b647302690f7a0d7a5f1a133107e4d5ece9fd
Tshepiso Tsotetsi <tshepisot@mr-t.af.didata.local>
2015-09-04 08:33:35 +0200

    removing presence_of validation from AlbumTrack class

--- app/models/album_track.rb
+++ app/models/album_track.rb
@@ -1,5 +1,4 @@
 class AlbumTrack < ActiveRecord::Base
   belongs_to :track
   belongs_to :album
-  validates_presence_of :track_id, :album_id
 end
--- spec/models/album_track_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe AlbumTrack, type: :model do
-  subject {AlbumTrack.new}
-  it { subject.attributes.each { |attr| expect(subject).to validate_presence_of attr[0] unless ["id", "created_at", "updated_at"].include?(attr[0]) } }
-end
