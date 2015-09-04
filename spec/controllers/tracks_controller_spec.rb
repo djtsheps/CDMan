c8be66821ffe611541a546a58cf05c09987b065c
Tshepiso Tsotetsi <tshepisot@mr-t.af.didata.local>
2015-09-04 14:46:16 +0200

    adding show tests

--- spec/controllers/tracks_controller_spec.rb
+++ spec/controllers/tracks_controller_spec.rb
@@ -7,11 +7,19 @@ RSpec.describe Api::V1::TracksController, type: :controller do
 
  let(:valid_session) { {} }
                                                                                                                                                                                                                                                                                 
- describe "GET #/api/tracks" do
+ describe "GET #/api/tracks => index" do
     it "assigns all first tracks as @tracks" do
       tracks = Track.create! valid_attributes                                                                                                                                                                                                                                 
       get :index, 'api/tracks'
       expect(assigns(:tracks)).to eq([tracks])
     end 
+
+  describe "GET #/api/tracks/1 => show" do
+    it "assigns first track as @track" do
+      track = Track.create valid_attributes
+      get :show, {controller: 'api/v1/tracks', action: 'show', id: track.id}
+      expect(assigns(:track)).to eq(track)
+    end 
+  end
   end 
 end
