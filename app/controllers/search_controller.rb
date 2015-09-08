dbf4d884ec180529e13f448e4b31b6fc227516eb
Tshepiso Tsotetsi <tshepisot@Mr-T.local>
2015-09-08 08:12:44 +0200

    Adding search query check and line counts

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,18 @@
 class SearchController < ApplicationController
   def index
-  	@albums = Album.where("LOWER(title) like LOWER(:title)",title: "%#{params[:query]}%")
-    @albums_message = "#{@albums.count} album(s) found..."
+  	@artists = @albums = @tracks = []
 
-    @tracks = Track.where("LOWER(title) like LOWER(:title)",title: "%#{params[:query]}%")                                                                                                                                                                                          
+    if params[:query]
+      if params[:query].length < 3
+       flash.now[:error] = "Please enter a more specific search query... (at least 3 characters)"
+      else
+        @albums = Album.where("LOWER(title) like LOWER(:title)",title: "%#{params[:query]}%")
+    	@tracks = Track.where("LOWER(title) like LOWER(:title)",title: "%#{params[:query]}%")
+    	@artists = Artist.where("LOWER(firstname) like LOWER(:query) OR LOWER(lastname) like LOWER(:query) OR LOWER(aka) like LOWER(:query)",query: "%#{params[:query]}%")
+      end
+    end
     @tracks_message = "#{@tracks.count} track(s) found..."
-
-     @artists = Artist.where("LOWER(firstname) like LOWER(:query) OR LOWER(lastname) like LOWER(:query) OR LOWER(aka) like LOWER(:query)",query: "%#{params[:query]}%")
     @artists_message = "#{@artists.count} artist(s) found..."
+    @albums_message = "#{@albums.count} album(s) found..."
   end
 end
